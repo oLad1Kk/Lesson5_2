3a5cb406a60fbf50141a1f63573c24109075a2d6
Vlad <vasvladikus@gmail.com>
2024-10-03 16:49:56 +0200

    Buy.cpp

--- Lesson5_2/Buy.cpp
+++ Lesson5_2/Buy.cpp
@@ -2,31 +2,41 @@
 
 Buy::Buy()
 {
+    amount = 1;
 }
 
-Buy::Buy(Product p, int amount)
+Buy::Buy(Product p, int amount) : Product(p)
 {
+    this->amount = amount;
 }
 
 void Buy::setAmount(int amount)
 {
+    this->amount = amount;
 }
 
 int Buy::getAmount() const
 {
-    return 0;
+    return amount;
 }
 
 double Buy::calcTotalPrice() const
 {
-    return 0.0;
+    return amount*getPrice();
 }
 
 double Buy::calcTotalWeight() const
 {
-    return 0.0;
+    return amount*getWeight();
 }
 
 void Buy::showBuy() const
 {
+    Product::showProduct();
+    cout << "Amount :" << amount << endl;
+    cout << "+++++++++++++++++++++++++++++++++++++++++++\n";
+    cout << "Total Price: " << calcTotalPrice() << endl;
+    cout << "Total Weight: " << calcTotalWeight() << endl;
+    cout << "+++++++++++++++++++++++++++++++++++++++++++\n";
+
 }
