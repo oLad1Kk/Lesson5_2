aceaa52fc80bebc68c2036b845091c38508cbb2f
Vlad <vasvladikus@gmail.com>
2024-10-03 16:37:50 +0200

    Product.cpp

--- Lesson5_2/Product.cpp
+++ Lesson5_2/Product.cpp
@@ -2,39 +2,53 @@
 
 Product::Product()
 {
+    name = "undefined";
+    price = 0;
+    weight = 0;
 }
 
 Product::Product(string name, double price, double weight)
 {
+    this->name = name;
+    this->price = price;
+    this->weight = weight;
 }
 
 void Product::setName(string name)
 {
+    this->name = name;
 }
 
 void Product::setPrice(double price)
 {
+    this->price = price;
+
 }
 
 void Product::setWeight(double weight)
 {
+    this->weight = weight;
+
 }
 
 string Product::getName() const
 {
-    return string();
+    return name;
 }
 
 double Product::getPrice() const
 {
-    return 0.0;
+    return price;
 }
 
 double Product::getWeight() const
 {
-    return 0.0;
+    return weight;
 }
 
 void Product::showProduct() const
 {
+    cout << "Name: " << name << endl;
+    cout << "Price: " << price << endl;
+    cout << "Weight: " << weight << endl;
 }
