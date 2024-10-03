b9455dd41174d07d38930c9f28a1469751481837
Vlad <vasvladikus@gmail.com>
2024-10-03 16:54:10 +0200

    main 2

--- Lesson5_2/Lesson5_2.cpp
+++ Lesson5_2/Lesson5_2.cpp
@@ -1,10 +1,13 @@
 #include <iostream>
 #include "Product.h"
+#include "Buy.h"
 
 int main()
 {
     Product product1("Banana", 10.1, 0.5);
-    product1.showProduct();
+    //product1.showProduct();
+    Buy B(product1, 5);
+    B.showBuy();
 }
 
 
