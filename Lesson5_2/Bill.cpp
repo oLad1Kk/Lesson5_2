cba3b975bac15c308451df3ecf0bbaf7ab179a06
Vlad <vasvladikus@gmail.com>
2024-10-03 17:00:38 +0200

    Bill.cpp

--- Lesson5_2/Bill.cpp
+++ Lesson5_2/Bill.cpp
@@ -1,9 +1,28 @@
 #include "Bill.h"
 
-Bill::Bill(Buy b)
+Bill::Bill(Buy b) : Buy(b)
 {
+
 }
 
 void Bill::printBill(string fileName)
 {
+	ofstream file(fileName);
+	if (file.is_open())
+	{
+		file << "Name: " << getName() << endl;
+		file << "Price: " << getPrice() << endl;
+		file << "Weight: " << getWeight() << endl;
+		file << "Amount :" << getAmount() << endl;
+		file << "+++++++++++++++++++++++++++++++++++++++++++\n";
+		file << "Total Price: " << calcTotalPrice() << endl;
+		file << "Total Weight: " << calcTotalWeight() << endl;
+		file << "+++++++++++++++++++++++++++++++++++++++++++\n";
+		file.close();
+		cout << "The bill was printed\n";
+	}
+	else
+	{
+		cout << "Erorr\n";
+	}
 }
